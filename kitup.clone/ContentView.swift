c7cf183cfe2acce998cbc09e9d8a1c3f72915ace
oguzhanaslann <oguz21199@gmail.com>
2022-02-10 22:04:47 +0300

    Initial Commit

---
+++ kitup.clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kitup.clone
+//
+//  Created by Oğuzhan Aslan on 10.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kitup.clone/kitup_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitup_cloneApp.swift
+//  kitup.clone
+//
+//  Created by Oğuzhan Aslan on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct kitup_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
